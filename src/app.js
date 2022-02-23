8910fa9d7a4145ac3f0484966b8625424e108223
Makar Solomatin <makar.solomatin.5@yandex.ru>
2022-02-23 14:12:23 +0300

    Add cors middleware

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import Body from 'koa-body';
 import Logger from 'koa-logger';
 import config from './config.js';
 import router from './api/root.js';
+import cors from '@koa/cors';
 
 new Koa()
   .use(new Body())
@@ -11,6 +12,7 @@ new Koa()
   .use(bearerToken())
   .use(router.routes())
   .use(router.allowedMethods())
+  .use(cors())
   .listen(config.port, config.host, () => {
     console.log(`listening on ${config.host}:${config.port}`);
   });
